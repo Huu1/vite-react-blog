7db46cd21b4a27a6a5bdf53aa738502b753d51e7
Hu Yao <593119798@qq.com>
2023-02-05 16:57:28 +0800

    2

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,7 +3,7 @@ import "./index.less";
 const Home = () => {
 	return (
 		<div className="home">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<h1 className="text-3xl font-bold underline">Helssslo world!</h1>
 		</div>
 	);
 };
